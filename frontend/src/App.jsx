143142f1ab4829f7afde8301f9188c14de2cc6ae
gomesfilipe <filipejvmf@gmail.com>
2022-09-02 21:14:33 -0300

    Páginas estilizadas.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// import React, { useState } from "react";
+import React from "react";
+import './global.css';
+import Router from './Router';
+
+function App() {
+  return (
+    <Router />
+  );
+}
+
+export default App;
